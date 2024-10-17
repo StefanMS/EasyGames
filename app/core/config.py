c932858be2fa79022e914ea7f95d8108bbfa0142
StefanMS <stefan_statescu@yahoo.com>
2024-10-17 15:53:01 +0300

    fixed envs, async

--- app/core/config.py
+++ app/core/config.py
@@ -4,23 +4,26 @@ Config module
 # pylint: disable=no-name-in-module
 # pylint: disable=no-self-argument
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
 
 
 class Settings(BaseSettings):
     '''
     Class for db settings
     '''
-    DATABASE_URL: str = "postgresql://admin:admin@localhost/easygames_db"
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str = os.getenv('DATABASE_URL')
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    ALGORITHM: str = os.getenv('ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+    ADMIN_USER_EMAIL: str = os.getenv('ADMIN_USER_EMAIL')
+    ADMIN_USER_PASSWORD: str = os.getenv('ADMIN_USER_PASSWORD')
+    ADMIN_USER_FIRST_NAME: str = os.getenv('ADMIN_USER_FIRST_NAME')
+    ADMIN_USER_LAST_NAME: str = os.getenv('ADMIN_USER_LAST_NAME')
 
-    ADMIN_USER_EMAIL: str
-    ADMIN_USER_PASSWORD: str
-    ADMIN_USER_FIRST_NAME: str
-    ADMIN_USER_LAST_NAME: str
-
-    BUCKET_URL: str = "https://zdybzhhqrimkhrbljuvs.supabase.co/storage/v1/object/public/GamesBucket"
+    BUCKET_URL: str = os.getenv('BUCKET_URL')
 
     class Config:
         env_file = ".env"
